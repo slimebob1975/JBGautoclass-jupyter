a08e84c2091eda096b4021f606d2da9873780e02
Robert Granat <robert.granat@iaf.se>
2022-05-17 09:00:39 +0200

    Remove more information from code cont

--- sql_config_sample.py
+++ sql_config_sample.py
@@ -2,7 +2,5 @@ DEFAULT_HOST = "the ip address of the local SQL Server"
 DEFAULT_ODBC_DRIVER = "the preferred ODBC driver" # "SQL Server" will do in most cases
 DEFAULT_DATA_CATALOG = "the database holding the data table where I want to start working" # E.g. "dbo.my_schema"
 DEFAULT_DATA_TABLE = "the table holding the data where I want to start working" # E.g. "iris"
-DEFAULT_DATA_COLUMNS = "the columns in the data table where I want to start working" # E.g. "sepal-width,petal-length"
-DEFAULT_CLASS_COLUMN = "the name of the column that gives the different data classes" # E.g. "class"
 DEFAULT_CLASSIFICATION_CATALOG = "the database where to store the classification results" # E.g. "dbo"
 DEFAULT_CLASSIFICATION_TABLE = "the table where to store the classification results" # E.g. "Autoclassification"
